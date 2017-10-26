fa0367a09618f29a23ddccde80b6852d5b5be57d
gdziuba <gmdziuba@gmail.com>
2017-10-26 15:41:29 -0400

    directory misspelled
    directory misspelled

--- http-event-collector/http-event-collector.js
+++ http-event-collector/http-event-collector.js
@@ -24,7 +24,7 @@ module.exports = function(RED) {
 
 
         // Change to require("splunk-logging").Logger;
-        var SplunkLogger = require("/../node_modeules/splunk-bunyan-logger/index").Logger;
+        var SplunkLogger = require("/../node_modules/splunk-bunyan-logger/index").Logger;
 
 
         /**
