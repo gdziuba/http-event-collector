4c90eda3ec96c82af0ed5c304d0b58a947dc13e1
Grey Dziuba <gmdziuba@gmail.com>
2024-01-12 09:02:27 -0500

    Adjustments from rXc3NtR1c

--- http-event-collector/http-event-collector.js
+++ http-event-collector/http-event-collector.js
@@ -1,6 +1,5 @@
 var SplunkLogger = require("splunk-logging").Logger;
 
-
 module.exports = function(RED) {
     function HTTPEventCollector(config) {
         RED.nodes.createNode(this,config);
@@ -9,6 +8,12 @@ module.exports = function(RED) {
         var myMessage = null;
 
 
+        RED.nodes.createNode(this, config);
+
+        var context = this.context();
+        var node = this
+        var server = RED.nodes.getNode(config.server);
+        var message = null;
 
         /**
          * Only the token property is required.
@@ -23,6 +28,14 @@ module.exports = function(RED) {
         var config = {
             token: this.myToken,
             url: this.myURI
+        this.SourceType = server.SourceType;
+        this.Host = (config.Host.toString() != "") ? config.Host.toString() : server.Host;
+        this.Source = (config.Source.toString() != "") ? config.Source.toString() : server.Source;
+        this.Index = (config.Index.toString() != "") ? config.Index.toString() : server.Index;
+
+        var splunkConfig = {
+            token: server.Token,
+            url: server.URI
         };
 
 
@@ -31,47 +44,73 @@ module.exports = function(RED) {
 
             // Create a new logger
             var Logger = new SplunkLogger(config);
-            
+            var Logger = new SplunkLogger(splunkConfig);
+
             Logger.error = function(err, context) {
                 // Handle errors here
                 console.log("error", err, "context", context);
+                if (server.LogConsole == true)
+                    console.log("error", err, "context", context);
             };
 
+            // set the log level if it is part of the message
+            var level = msg.LogLevel;
+            if(level == undefined) {
+                level = server.LogLevel;
+            } else {
+                // remove from logged message
+                delete msg.LogLevel;
+            }
 
             // Attempt to convert msg.payload to a json structure.
             try{
                 myMessage = JSON.parse(msg)
+                message = JSON.parse(msg)
             }
             catch(err){
                 myMessage = msg
+                message = msg
             }
 
             var payload = {
                 // Data sent from previous node msg.payload
                 message: myMessage,                
                 //msgMetaData : msg,
+                message: message,
                 // Metadata
                     metadata: {
                         source: this.mySource,
                         sourcetype: this.mySourcetype,
                         index: this.myIndex,
                         host: this.myHost,
+                        source: this.Source,
+                        sourcetype: this.SourceType,
+                        index: this.Index,
+                        host: this.Host,
                     },
                     // Severity is also optional
                 severity: "info"
-            
+                severity: level
+
             };
 
-            
+
 
             console.log("Sending payload", payload);
+            if (server.LogConsole == true)
+                console.log("Sending payload", payload);
+                
             Logger.send(payload, function(err, resp, body) {
                 // If successful, body will be { text: 'Success', code: 0 }
                 console.log("Response from Splunk", body);
+                if (server.LogConsole == true)
+                    console.log("Response from Splunk", body);
             });
 
 
         });
     }
     RED.nodes.registerType("splunk-http-event-collector",HTTPEventCollector);
+};
+    RED.nodes.registerType("splunk-http-event-collector", HTTPEventCollector);
 };
---
+++ http-event-collector/http-event-config.js
@@ -0,0 +1,14 @@
+module.exports = function(RED) {
+    function HTTPEventConfig(c) {
+        RED.nodes.createNode(this,c);
+        this.URI = c.URI;
+        this.Token = c.Token;
+        this.SourceType = c.SourceType;
+        this.Host = c.Host;
+        this.Source = c.Source;
+        this.Index = c.Index;
+        this.LogLevel = c.LogLevel;
+        this.LogConsole = c.LogConsole;
+    }
+    RED.nodes.registerType("splunk-http-event-config", HTTPEventConfig);
+};
